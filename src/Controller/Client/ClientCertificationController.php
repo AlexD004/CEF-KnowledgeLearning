5136f4560412d1ab4d303f7d7b87f766af96b43d
AlexD004 <dupre.alexandre04@gmail.com>
2025-07-04 22:47:21 +0200

    add pdf for certif

--- src/Controller/Client/ClientCertificationController.php
+++ src/Controller/Client/ClientCertificationController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\UserCertification;
+use Mpdf\Mpdf;
 
 /**
  * Controller handling the display of user certifications (validated cursus).
@@ -34,5 +35,57 @@ class ClientCertificationController extends AbstractController
         return $this->render('client/userCertificates.html.twig', [
             'certifications' => $certifications,
         ]);
+    }
+
+    /**
+     * Generates a downloadable PDF certification for the authenticated user.
+     *
+     * This route is restricted to the user who owns the certification.
+     * The generated PDF includes user's name, cursus name, and date of completion.
+     *
+     * @param UserCertification $certification The certification entity (injected from route)
+     * @return Response A PDF file rendered inline in the browser
+     */
+    #[Route('/apprenant/certificats/{id}/pdf', name: 'client_certificates_pdf')]
+    public function generateCertificatePdf(UserCertification $certification): Response
+    {
+        $user = $this->getUser();
+
+        // Security check: ensure the logged-in user owns this certification
+        if ($certification->getUser() !== $user) {
+            throw $this->createAccessDeniedException("Ce certificat ne vous appartient pas.");
+        }
+
+        // Render HTML template for the PDF
+        $html = $this->renderView('client/certificate_pdf.html.twig', [
+            'user' => $user,
+            'certification' => $certification,
+        ]);
+
+        // Configure mPDF
+        $mpdf = new Mpdf([
+            'format' => 'A4',
+            'orientation' => 'P',
+            'tempDir' => __DIR__ . '/../../var/tmp', 
+        ]);
+
+        $mpdf->WriteHTML($html); 
+
+        // Generate a filename
+        /** @var \App\Entity\Cursus $cursus */
+        $cursus = $certification->getCursus();
+        /** @var \App\Entity\User $owner */
+        $user = $certification->getUser();
+
+        $filename = 'certificat_' . $cursus->getId() . '_' . $user->getId() . '.pdf';
+
+        // Return the PDF as an inline response
+        return new Response(
+            $mpdf->Output($filename, 'I'), // 'I' for inline display in browser
+            200,
+            [
+                'Content-Type' => 'application/pdf',
+            ]
+        );
     }
 }
--- src/Entity/UserCertification.php
+++ src/Entity/UserCertification.php
@@ -6,53 +6,104 @@ use App\Repository\UserCertificationRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use App\Entity\Traits\TimestampableEntity;
+use App\Entity\Cursus;
+use App\Entity\User;
 
+/**
+ * Entity representing a validated cursus for a user.
+ *
+ * This certification is generated when a user completes all lessons
+ * of a given cursus. It includes the date of validation (via createdAt).
+ */
 #[ORM\Entity(repositoryClass: UserCertificationRepository::class)]
 #[ORM\HasLifecycleCallbacks]
 #[ORM\Table(name: 'user_certification')]
 class UserCertification
 {
-
     use TimestampableEntity;
 
+    /**
+     * Unique identifier of the certification record.
+     *
+     * @var int|null
+     */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
+    /**
+     * The user who completed the cursus.
+     *
+     * @var User|null
+     */
     #[ORM\ManyToOne(targetEntity: User::class)]
     #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
     #[Assert\NotNull]
     private ?User $user = null;
 
+    /**
+     * The cursus that has been validated.
+     *
+     * @var Cursus|null
+     */
     #[ORM\ManyToOne(targetEntity: Cursus::class)]
     #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
     #[Assert\NotNull]
     private ?Cursus $cursus = null;
 
-    // ===== Getters & Setters =====
+    // ========================
+    // Getters & Setters
+    // ========================
 
+    /**
+     * Get the certification ID.
+     *
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * Get the user who owns the certification.
+     *
+     * @return User|null
+     */
     public function getUser(): ?User
     {
         return $this->user;
     }
 
+    /**
+     * Set the user who owns the certification.
+     *
+     * @param User|null $user
+     * @return self
+     */
     public function setUser(?User $user): self
     {
         $this->user = $user;
         return $this;
     }
 
+    /**
+     * Get the cursus that has been validated.
+     *
+     * @return Cursus|null
+     */
     public function getCursus(): ?Cursus
     {
         return $this->cursus;
     }
 
+    /**
+     * Set the cursus that has been validated.
+     *
+     * @param Cursus|null $cursus
+     * @return self
+     */
     public function setCursus(?Cursus $cursus): self
     {
         $this->cursus = $cursus;
